6dc41890b4d776393b92f0f845b856ced4f63b7c
kinqbert <perehodko1010@gmail.com>
2024-12-16 04:15:34 +0200

    refactor API routes for cars to include a consistent path structure

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -4,13 +4,13 @@ export const API_ROUTES = {
     LOGIN: "/api/auth/login",
   },
   CARS: {
-    GET_USER_CARS: "/api/user_cars",
+    GET_USER_CARS: "/api/cars/user_cars",
     GET_ALL_CARS: "/api/cars",
     GET_CAR_BY_ID: (id: string) => `/api/cars/${id}`,
-    PURCHASE: (id: string) => `/api/${id || ""}/purchase`,
-    SELL: (id: string) => `/api/${id || ""}/sell`,
-    CANCEL_SELL: (id: string) => `/api/${id || ""}/cancel_sell`,
-    REPAIR: (id: string) => `/api/${id || ""}/repair`,
+    PURCHASE: (id: string) => `/api/cars/${id || ""}/purchase`,
+    SELL: (id: string) => `/api/cars/${id || ""}/sell`,
+    CANCEL_SELL: (id: string) => `/api/cars/${id || ""}/cancel_sell`,
+    REPAIR: (id: string) => `/api/cars/${id || ""}/repair`,
     CREATE: "/api/cars/create",
   },
   USER: {
